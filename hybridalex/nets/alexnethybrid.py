6718255368738111fc38e32f64234d454665519e
huanyuzh <huanyuzh@umich.edu>
2017-11-08 15:41:27 -0500

    Update alexnethybrid.py

--- hybridalex/nets/alexnethybrid.py
+++ hybridalex/nets/alexnethybrid.py
@@ -59,5 +59,68 @@ def original(images, labels, num_classes, total_num_examples, devices=None, is_t
 
 
 def ndev_data(images, labels, num_classes, total_num_examples, devices, is_train=True):
-    # Your code starts here...
-    return
+    """Build inference, data parallelism"""
+    # use the last device in list as variable device
+    devices = devices[:]
+    builder = ModelBuilder(devices.pop())
+
+    if not is_train:
+        with tf.variable_scope('model'):
+            prob = alexnet_inference(builder, images, labels, num_classes)[0]
+        return alexnet_eval(prob, labels)
+
+    # configure optimizer
+    def configure_optimizer(global_step, total_num_steps):
+        """Return a configured optimizer"""
+        def exp_decay(start, tgtFactor, num_stairs):
+            decay_step = total_num_steps / (num_stairs - 1)
+            decay_rate = (1 / tgtFactor) ** (1 / (num_stairs - 1))
+            return tf.train.exponential_decay(start, global_step, decay_step, decay_rate,
+                                              staircase=True)
+
+        def lparam(learning_rate, momentum):
+            return {
+                'learning_rate': learning_rate,
+                'momentum': momentum
+            }
+
+        return HybridMomentumOptimizer({
+            'weights': lparam(exp_decay(0.001, 250, 4), 0.9),
+            'biases': lparam(exp_decay(0.002, 10, 2), 0.9),
+        })
+
+    with tf.device(builder.variable_device()):
+        global_step = builder.ensure_global_step()
+    opt = configure_optimizer(global_step, total_num_examples)
+
+    # construct each replica
+    replica_grads = []
+    with tf.device(builder.variable_device()):
+        image_slices = tf.split(0, len(devices), images)
+        label_slices = tf.split(0, len(devices), labels)
+    with tf.variable_scope('model') as vsp:
+        # we only want scope for variables but not operations
+        with tf.name_scope(''):
+            for idx in range(len(devices)):
+                dev = devices[idx]
+                with tf.name_scope('tower_{}'.format(idx)) as scope:
+                    with tf.device(dev):
+                        prob, logits, total_loss = alexnet_inference(builder, image_slices[idx],
+                                                                     label_slices[idx], num_classes,
+                                                                     scope)
+                        # calculate gradients for batch in this replica
+                        grads = opt.compute_gradients(total_loss)
+
+                replica_grads.append(grads)
+                # reuse variable for next replica
+                vsp.reuse_variables()
+
+    # average gradients across replica
+    with tf.device(builder.variable_device()):
+        grads = builder.average_gradients(replica_grads)
+        apply_grads_op = opt.apply_gradients(grads, global_step=global_step)
+
+        train_op = tf.group(apply_grads_op, name='train')
+
+    # simply return prob, logits, total_loss from the last replica for simple evaluation
+    return prob, logits, total_loss, train_op, global_step
